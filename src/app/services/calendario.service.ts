04c7a7a6e1a97cb3f160e5c72a6f6c2981726818
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-04-09 13:38:02 -0300

    Creacion del servicio calendario y sus metodos GET/POST/DELETE

---
+++ src/app/services/calendario.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { ResponseApi } from '../models/response-api';
+import { Calendario } from '../models/calendario';
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarioService {
+
+  constructor(private http:HttpClient) { }
+
+
+  urlApi:string = 'https://localhost:7026/api/calendario';
+
+  httpOptions = {headers: new HttpHeaders({
+    "Content-Type":"application/json"
+  })}
+
+  mostrarCalendario():Observable<ResponseApi>{
+
+
+    return this.http.get<ResponseApi>(this.urlApi,this.httpOptions);
+  }
+
+  crearCalendario(calendario:Calendario):Observable<ResponseApi>{
+
+    return this.http.post<ResponseApi>(this.urlApi,calendario,this.httpOptions);
+  }
+
+  eliminarCalendario(id:number):Observable<ResponseApi>{
+
+    return this.http.delete<ResponseApi>(`${this.urlApi}/${id}`,this.httpOptions);
+  }
+
+
+}
