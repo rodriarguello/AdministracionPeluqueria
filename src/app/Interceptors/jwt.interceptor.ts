ab65aec384f5c3da27fe4da5ae127ba5738cb3bf
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-05 22:34:46 -0300

    Modificacion de jwtInterceptor, para que cuando una petición devuelva un codigo 401 redireccione al login

--- src/app/Interceptors/jwt.interceptor.ts
+++ src/app/Interceptors/jwt.interceptor.ts
@@ -3,15 +3,17 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { UsuariosService } from '../services/usuarios.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor(private usuarioService:UsuariosService) {}
+  constructor(private usuarioService:UsuariosService, private router:Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
    const credencialesUsuario = this.usuarioService.credencialesUsuario;
@@ -19,13 +21,26 @@ export class JwtInterceptor implements HttpInterceptor {
     request = request.clone({
 
       setHeaders:{
-        Authorization : `Bearer ${credencialesUsuario.token}`
+        Authorization : `Bearer ${JSON.parse(localStorage.getItem('token')!).token}`
       }
 
     });
    }
    
    
-    return next.handle(request);
+    return next.handle(request).pipe(
+      tap((event: HttpEvent<any>) => {
+        
+        
+        return event;
+      }, 
+      (error)=>{
+        if(error.status ==401){
+          
+          this.router.navigate(['index']);
+
+        }
+      })
+    );
   }
 }
