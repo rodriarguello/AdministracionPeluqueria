385689920c1e9dbbac90cb77198032257be6edd1
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-22 18:26:08 -0300

    Modificación de ruta de api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment ={
     production:true,
-    API_URL: 'http://rodriarguello96-001-site1.htempurl.com/'
+    API_URL: 'http://rodriarguello96-001-site1.htempurl.com'
     
 }
