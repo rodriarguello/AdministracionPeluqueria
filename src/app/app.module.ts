4caa78a0be44116424b3bc4ade076a2753f6133f
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-24 13:41:16 -0300

    Se modifica donde se llama a la clase que modifica al matPaginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { JwtInterceptor } from './Interceptors/jwt.interceptor';
 import { SpinnerInterceptor } from './Interceptors/spinner.interceptor';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatPaginatorIntl } from '@angular/material/paginator';
-import { CustomMatPaginatorIntl } from './Reutilizable/MatPaginatorInt';
 
 @NgModule({
   declarations: [
@@ -31,8 +29,7 @@ import { CustomMatPaginatorIntl } from './Reutilizable/MatPaginatorInt';
     UsuariosService,
     UtilidadService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true},
-    {provide:HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi:true},
-    {provide: MatPaginatorIntl, useClass: CustomMatPaginatorIntl}
+    {provide:HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi:true}
   ],
   
   bootstrap: [AppComponent]
